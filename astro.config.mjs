fa5295c6aa84ba293d64b6e4e42ea4a91c404775
Matteo Papadopoulos <m.papadopoulos@cantierecreativo.net>
2025-10-26 23:07:56 +0100

    Nuovi file cloudflare

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,12 @@
 import { defineConfig, envField } from 'astro/config';
 
 import tailwind from '@astrojs/tailwind';
-import node from '@astrojs/node';
+import cloudflare from '@astrojs/cloudflare';
 
 // https://astro.build/config
 export default defineConfig({
   output: 'server',
-  adapter: node({
-    mode: 'standalone',
-  }),
+  adapter: cloudflare(),
   env: {
     schema: {
       DATOCMS_PUBLISHED_CONTENT_CDA_TOKEN: envField.string({
